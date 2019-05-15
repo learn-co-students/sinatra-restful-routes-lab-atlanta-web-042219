bf7a097ca3811bfa04ea0a53ea38e48bc5533d0b
Andrew Allen <asallen67@gmail.com>
2019-05-15 10:10:21 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,52 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  #__________ home __________
 
+  get '/' do
+  	redirect '/recipes'
+  end
+
+  get '/recipes' do
+  	erb :index
+  end
+
+  #__________ create / new __________
+
+  get '/recipes/new' do
+    erb :new_recipe
+  end
+
+  post '/recipes' do
+    redirect "/recipes/#{Recipe.create(params).id}"
+  end
+
+  # get
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    erb :show_recipe
+  end
+
+  # edit / update / patch
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
+    erb :edit_recipe
+  end
+
+  patch '/recipes/:id' do
+  	params.delete('_method')
+  	puts params
+  	Recipe.update(params[:id], params)
+  	redirect "recipes/#{params[:id]}"
+  end
+
+  # delete
+
+  delete '/recipes/:id' do
+  	Recipe.destroy(params[:id])
+  	redirect '/recipes'
+  end
+#
 end
---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+Recipe.create(name: "Test_1", cook_time: "0 minutes", ingredients: "lotsastuff")
+Recipe.create(name: "Test_2", cook_time: "0 minutes", ingredients: "lotsastuff")
+Recipe.create(name: "Test_3", cook_time: "0 minutes", ingredients: "lotsastuff")
+Recipe.create(name: "Test_4", cook_time: "0 minutes", ingredients: "lotsastuff")
