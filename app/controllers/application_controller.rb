f82fbf261b2036368ec246f7ff87524f6a1920f4
Will Jones <willjones2010@gmail.com>
2019-05-15 16:36:28 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,50 @@
 class ApplicationController < Sinatra::Base
+  # set :method_override, true
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get '/' do
+    redirect '/recipes'
+  end
+
+  get '/recipes' do
+    erb :index
+  end
+
+  get '/recipes/new' do
+    erb :new
+  end
+
+  post '/recipes' do
+    @recipe = Recipe.create(params)
+    redirect to "recipes/#{@recipe.id}"
+  end
+
+  get "/recipes/:id/edit" do
+    @recipe = Recipe.find(params[:id])
+    erb :edit
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  end
+
+  patch '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    params.delete("_method")
+    @recipe.update(params)
+    redirect to "/recipes/#{@recipe.id}"
+
+  end
+
+  delete '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.destroy
+    # Recipe.destroy(params[:id])
+    redirect to '/recipes'
+  end
 
 end
